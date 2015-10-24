92123708329d786afcd1ed7fba35ffc0c84831db
Xab3r <mail.xaber@gmail.com>
2015-10-25 00:46:23 +0300

    Added empty tests project

--- WpfAutoCompleteControls/WpfAutoCompleteControls/Converters/ExceptionToMessageConverter.cs
+++ WpfAutoCompleteControls/WpfAutoCompleteControls/Converters/ExceptionToMessageConverter.cs
@@ -9,12 +9,7 @@
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var exception = value as Exception;
-            if (exception == null)
-            {
-                return null;
-            }
-
-            return exception.Message;
+            return exception?.Message;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
